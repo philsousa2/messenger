e6219d06be9fddd8cdb9158b74d3de31606c3cbb
philsousa2 <philsousa28@gmail.com>
2022-08-31 16:51:41 -0300

    ajuda ajuste

---
+++ styles/pages/ajuda.js
@@ -0,0 +1,15 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+background: #FFF;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 30px;
+flex-direction: column;
+
+.accordion{
+    width: 70%;
+}
+`
