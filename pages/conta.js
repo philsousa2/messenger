5b8df85828c64e6a8e069015dfef64b505c5357c
philsousa2 <philsousa28@gmail.com>
2022-09-03 15:42:00 -0300

    pg abra sua conta

--- pages/conta.js
+++ pages/conta.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Container } from "../styles/pages/conta";
 import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 
 function conta() {
@@ -12,6 +11,36 @@ function conta() {
             A Messenger não fatura despesas referentes às taxas e tributos gerados na importação 
             de remessa expressa. Preencha aqui as informações cadastrais da sua empresa, para aprovação 
             do crédito. Nossa Central de Serviços retornará o contato em até dois dias.</p>
+            <div className="formulario">
+              <h2>Todos os campos são obrigatórios</h2>
+        <TextField className="input2" id="cnpj" label="CNPJ" required />
+        <TextField className="input2" id="razaoSocial" label="Razão Socia" required />
+        <br/>
+        <TextField className="input2" id="inscricaoEstadual" label="Inscrição Estadual" required />
+        <TextField className="input2" id="cnae" label="CNAE" required />
+        <br/>
+        <TextField className="input" id="nome" label="Nome do Responsável" required />
+        <br/>
+        <TextField className="input2" id="email" label="Email do Responsável" required />
+        <TextField className="input2" id="telefone" label="Telefone para Contato" required />
+        <br/>
+        <TextField className="input2" id="emailFinanceiro" label="Email do Responsável Financeiro" required />
+        <TextField className="input2" id="nomeFinanceiro" label="Contato no Depto Financeiro" required />
+        <br/>
+        <TextField className="input3" id="cep" label="CEP" required />
+        <TextField className="input2" id="logradouro" label="Logradouro" required />
+        <TextField className="input3" id="numero" label="Número" required />
+        <br/>
+        <TextField className="input2" id="complemento" label="Complemento" required />
+        <TextField className="input3" id="bairro" label="Bairro" required />
+        <TextField className="input3" id="municipio" label="Município" required />
+        <br/>
+        <TextField className="input4" id="uf" label="UF" required />
+        <TextField className="input3" id="pais" label="País" required />
+        <Button  className="btn" variant="contained" disableElevation>
+                 Enviar
+            </Button>
+        </div>
     </Container>
    
 
--- styles/pages/conta.js
+++ styles/pages/conta.js
@@ -11,8 +11,104 @@ flex-direction: column;
 h1{
     color: #00457A;
 }
+p{
+    width: 70%;
+    margin-top: 30px;
+    text-align: center;
+}
+.formulario{
+    width: 90%;
+    box-shadow: -1px 1px 20px 4px rgba(0,0,0,0.32);
+    border-radius: 20px;
+    padding: 40px 200px 40px 200px;
+    background:  #F8F8FF;
+    margin: 40px 0 80px 0;
+    text-align: center;
+    color: #00457A;
+}
 
-@media(max-width: 750px) {
+.btn{
+    margin-top: 20px;
+    height: 55px;
+    width:50%;
+    margin-inline: 20px;
+}
+
+.input{
+    width: 84%;
+    margin-top: 20px;
+    margin-inline: 20px;
+}
+
+.input2{
+    width: 40%;
+    margin-top: 20px;
+    margin-inline: 20px;
+}
+
+.input3{
+    width: 20%;
+    margin-top: 20px;
+    margin-inline: 10px;
+}
+.input4{
+    width: 10%;
+    margin-top: 20px;
+    margin-inline: 10px;
+}
+
+
+@media(max-width: 1300px) {
+    .formulario{
+    padding: 40px 100px 40px 100px;
+}
+}
+
+@media(max-width: 1300px) {
+    .formulario{
+    padding: 40px 50px 40px 50px;
+}
+}
+
+@media(max-width: 900px) {
+    .formulario{
+    padding: 40px 10px 40px 10px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     
 }
+.btn{
+    width:90%;
+}
+
+.input{  
+    width: 90%;
+    margin-top: 15px;
+    margin-inline: 0;
+}
+
+.input2{
+    width: 90%;
+    margin-top: 15px;
+    margin-inline: 0;
+}
+
+.input3{
+    width: 90%;
+    margin-top: 15px;
+    margin-inline: 0;   
+}
+
+.input4{
+    margin-top: 15px;
+    width: 90%;
+    margin-inline: 0;
+}
+br {
+    display: none;
+}
+}
+
 `
