5437abcaf7f0f72a4286f38c855bf3bb64c48bfd
philsousa2 <philsousa28@gmail.com>
2022-09-06 17:52:22 -0300

    header ajustes

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Container } from "../styles/header";
+import { MdOutlineLocalShipping, MdOutlinePersonalInjury } from "react-icons/md";
+
 function Header() {
   return (
     <Container>
@@ -21,11 +23,15 @@ function Header() {
         <div className="LogoFinal"></div>
 
         <div className="Contatos">
+        <Image  src="/whatsapp-logo-png.png" alt="Logo" width={30} height={30} margin-left={50} />
+        <a href="https://api.whatsapp.com/send?phone=552121428080"  target="_blank" rel="noreferrer">   +55 (21) 2142-8000</a>
+        <a>|</a>
+        
+        <MdOutlineLocalShipping size={30}/>
+        <a href="https://web.archive.org/web/20220127053821/https://messenger.brudam.com.br/site/"  target="_blank" rel="noreferrer"> Portal do Cliente</a>
+        <a>|</a>
 
-        <a href="https://api.whatsapp.com/send?phone=552121428080"  target="_blank" rel="noreferrer">+55 (21) 2142-8000|</a>
-        <a>|</a>
-        <a href="https://web.archive.org/web/20220127053821/https://messenger.brudam.com.br/site/"  target="_blank" rel="noreferrer">Portal do Cliente</a>
-        <a>|</a>
+        <MdOutlinePersonalInjury size={30}/>
         <a href="https://web.archive.org/web/20220127053821/https://messenger.brudam.com.br/agente/loga.php"  target="_blank" rel="noreferrer">Portal do Agente</a>
       </div>
       </div>
