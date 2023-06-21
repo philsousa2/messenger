36a131b7b287600786513b343568b6dfeec010d6
philsousa2 <philsousa28@gmail.com>
2023-06-21 14:50:58 -0300

    correção

--- pages/rastreamento.js
+++ pages/rastreamento.js
@@ -2,20 +2,21 @@ import React, { useState } from 'react'
 import { Container } from "../styles/pages/rastreamento";
 
 
-
 function Rastreamento() {
+
   const [formulario, setFormulario] = useState({
     rastreamento: ""
    })
 
+ 
 
   const valorInput = e => setFormulario({ ...formulario, [e.target.name]: e.target.value})
 
   const enviarRastreamento = async e => {
     e.preventDefault();
+    console.log(formulario.rastreamento)
 
   {
-    
     const response = await fetch("/rastreamentoMessenger/", {
       method: 'POST',
       headers: {
@@ -23,24 +24,20 @@ function Rastreamento() {
       },
       body: JSON.stringify({formulario})
     })
-    const data = await response.json();
-    const result  = JSON.parse(data);
+    const result = await response.json();
     console.log(result);
- 
   }
-  
+
   }
 
   return (
-    
     <Container>
     <form className='formulario' onSubmit={enviarRastreamento}>
         <h1>Rastreamento</h1>
         <p>As informações exibidas no sistema de rastreamento são relativas aos locais onde as atualizações são feitas, não implicando, portanto, registro dos locais por onde as cargas transitam. Para mais informações, consulte a Central de Serviços.</p>
     <input type="text" className="input" name="rastreamento" placeholder="Minuta/ Nota Fiscal/ Pedido/ CT-e" onChange={valorInput} required />
-    <button  type="submit" className="btn"> Buscar </button>   
-    </form> 
-  
+    <button  type="submit" className="btn"> Buscar </button>
+    </form>
     </Container>
   )
 }
