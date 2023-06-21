4bf8f1d2ca135fae6bbc87b641c6a5c4a53cf368
philsousa2 <philsousa28@gmail.com>
2023-06-21 16:03:41 -0300

    teste de erro de numeração

--- pages/rastreamento.js
+++ pages/rastreamento.js
@@ -8,6 +8,7 @@ function Rastreamento() {
 
   const [dados, setDados] = useState([]);
   const [erro, setErro] = useState(null);
+  const [mostrarFormulario, setMostrarFormulario] = useState(true);
 
   const valorInput = e => setFormulario({ ...formulario, [e.target.name]: e.target.value });
 
@@ -28,42 +29,57 @@ function Rastreamento() {
       if (result.status === 1) {
         setDados(result.data[0].dados);
         setErro(null);
+        setMostrarFormulario(false);
       } else {
         setDados([]);
         setErro("Número não encontrado");
+        setMostrarFormulario(true);
       }
     } catch (error) {
       console.error(error);
       setDados([]);
       setErro("Ocorreu um erro ao buscar o rastreamento");
+      setMostrarFormulario(true);
     }
+  };
+
+  const novoRastreamento = () => {
+    setFormulario({ rastreamento: "" });
+    setDados([]);
+    setErro(null);
+    setMostrarFormulario(true);
   };
 
   return (
     <Container>
-      <form className='formulario' onSubmit={enviarRastreamento}>
-        <h1>Rastreamento</h1>
-        <p>As informações exibidas no sistema de rastreamento são relativas aos locais onde as atualizações são feitas, não implicando, portanto, registro dos locais por onde as cargas transitam. Para mais informações, consulte a Central de Serviços.</p>
-        <input type="text" className="input" name="rastreamento" placeholder="Minuta/ Nota Fiscal/ Pedido/ CT-e" onChange={valorInput} required />
-        <button type="submit" className="btn">Buscar</button>
-      </form>
-
-      <div>
-        {erro ? (
-          <p>{erro}</p>
-        ) : (
-          dados.map(item => (
-            <div key={item.id}>
-              <h2>{item.tipo}</h2>
-              <p>Número: {item.numero}</p>
-              <p>Status: {item.status}</p>
-              <p>Data: {item.data}</p>
-              <p>Descrição: {item.descricao}</p>
-              
-            </div>
-          ))
-        )}
-      </div>
+      {mostrarFormulario ? (
+        <form className='formulario' onSubmit={enviarRastreamento}>
+          <h1>Rastreamento</h1>
+          <p>As informações exibidas no sistema de rastreamento são relativas aos locais onde as atualizações são feitas, não implicando, portanto, registro dos locais por onde as cargas transitam. Para mais informações, consulte a Central de Serviços.</p>
+          <input type="text" className="input" name="rastreamento" placeholder="Minuta/ Nota Fiscal/ Pedido/ CT-e" onChange={valorInput} required />
+          <button type="submit" className="btn">Buscar</button>
+        </form>
+      ) : (
+        <>
+          {erro ? (
+            <div className="alerta">{erro}</div>
+          ) : (
+            <>
+              {dados.map(item => (
+                <div key={item.id}>
+                  <h2>{item.tipo}</h2>
+                  <p>Número: {item.numero}</p>
+                  <p>Status: {item.status}</p>
+                  <p>Data: {item.data}</p>
+                  <p>Descrição: {item.descricao}</p>
+                  
+                </div>
+              ))}
+              <button className="btn" onClick={novoRastreamento}>Realizar novo rastreamento</button>
+            </>
+          )}
+        </>
+      )}
     </Container>
   );
 }
