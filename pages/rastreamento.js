2ec42e2f9a11eb566e69d674b48735382100f7b1
philsousa2 <philsousa28@gmail.com>
2023-06-21 15:19:34 -0300

    teste de maluco

--- pages/rastreamento.js
+++ pages/rastreamento.js
@@ -1,47 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Container } from "../styles/pages/rastreamento";
 
-
 function Rastreamento() {
-
   const [formulario, setFormulario] = useState({
     rastreamento: ""
-   })
+  });
 
-   const [dados, setDados] = useState([]);
+  const [dados, setDados] = useState([]);
 
-  const valorInput = e => setFormulario({ ...formulario, [e.target.name]: e.target.value})
+  const valorInput = e => setFormulario({ ...formulario, [e.target.name]: e.target.value });
 
   const enviarRastreamento = async e => {
     e.preventDefault();
 
-  {
     const response = await fetch("/rastreamentoMessenger/", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({formulario})
-    })
+      body: JSON.stringify({ formulario })
+    });
+
     const result = await response.json();
     setDados(result.data[0].dados);
-    console.log(result);
-    console.log(dados);
-   
-  }
-
-  }
+  };
 
   return (
     <Container>
-    <form className='formulario' onSubmit={enviarRastreamento}>
+      <form className='formulario' onSubmit={enviarRastreamento}>
         <h1>Rastreamento</h1>
         <p>As informações exibidas no sistema de rastreamento são relativas aos locais onde as atualizações são feitas, não implicando, portanto, registro dos locais por onde as cargas transitam. Para mais informações, consulte a Central de Serviços.</p>
-    <input type="text" className="input" name="rastreamento" placeholder="Minuta/ Nota Fiscal/ Pedido/ CT-e" onChange={valorInput} required />
-    <button  type="submit" className="btn"> Buscar </button>   
-    </form>
+        <input type="text" className="input" name="rastreamento" placeholder="Minuta/ Nota Fiscal/ Pedido/ CT-e" onChange={valorInput} required />
+        <button type="submit" className="btn">Buscar</button>
+      </form>
+
+      <div>
+        {dados.map(item => (
+          <div key={item.id}>
+            <h2>{item.tipo}</h2>
+            <p>Número: {item.numero}</p>
+            <p>Status: {item.status}</p>
+            <p>Data: {item.data}</p>
+            <p>Descrição: {item.descricao}</p>
+            {/* Renderizar outras informações conforme necessário */}
+          </div>
+        ))}
+      </div>
     </Container>
-  )
+  );
 }
 
-export default Rastreamento
+export default Rastreamento;
