7b3064ddd542e281e2e165d4c12cb70d5ba618eb
philsousa2 <philsousa28@gmail.com>
2023-11-17 13:07:52 -0300

    a

--- styles/navbar.js
+++ styles/navbar.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   margin: auto;
   width: 100%;
-  height: 65px;
+  height: 45px;
   display: flex;
   align-items: center;
   justify-content: right;
