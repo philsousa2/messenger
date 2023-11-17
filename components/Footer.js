53cad168d07c268f77fae8e72a68f358a0813419
philsousa2 <philsousa28@gmail.com>
2023-11-17 14:33:56 -0300

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,12 +8,12 @@ function Footer() {
       <div>  
         <Image  src="/logobranca.png" alt="Logo" width={250} height={80} margin-left={50} />
       </div>
-      
+      <div className="linha2"></div>
       <div className="texto">
       <h3>Serviços de encomenda expressa nacional e internacional
         Exportação e importação pelo Regime de Tributação Simplificada</h3>
         <div className="linha"></div>
-        <h4>© Copyright 2022 by Messenger Express | Desenvolvido por Phelipe Sousa e Bruno Tubio</h4>
+        <h4>© Copyright 2022 by Messenger Express | Desenvolvido por Phelipe Sousa e Ramon Borzoni</h4>
       </div>
     </Container>
   )
