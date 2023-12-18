cea23a1656bb1d4da3fc98bfce5e0319c632a838
philsousa2 <philsousa28@gmail.com>
2023-12-18 12:00:40 -0300

    AAA

--- components/Banner.js
+++ components/Banner.js
@@ -19,64 +19,6 @@ const Banner = () => {
   }, []);
 
 
-// Adiciona uma classe para reiniciar a animação dos elementos com a classe 'TxtinfoH2'
-const h2Elements = document.querySelectorAll('.TxtinfoH2');
-h2Elements.forEach((h2) => {
-  h2.classList.remove('reset-animation');
-  void h2.offsetWidth; // Trigger reflow to restart the animation
-  h2.classList.add('reset-animation');
-});
-// Adiciona uma classe para reiniciar a animação dos elementos com a classe 'TxtinfoH2_002'
-const h2Elements02 = document.querySelectorAll('.TxtinfoH2002');
-h2Elements02.forEach((h202) => {
-  h202.classList.remove('reset-animation');
-  void h202.offsetWidth; // Trigger reflow to restart the animation
-  h202.classList.add('reset-animation');
-});
-// Adiciona uma classe para reiniciar a animação dos elementos com a classe 'TxtinfoH2_003'
-const h2Elements03 = document.querySelectorAll('.TxtinfoH2003');
-h2Elements03.forEach((h203) => {
-  h203.classList.remove('reset-animation');
-  void h203.offsetWidth; // Trigger reflow to restart the animation
-  h203.classList.add('reset-animation');
-});
-// Adiciona uma classe para reiniciar a animação dos elementos com a classe 'TxtinfoH2_004'
-const h2Elements04 = document.querySelectorAll('.TxtinfoH2004');
-h2Elements04.forEach((h204) => {
-  h204.classList.remove('reset-animation');
-  void h204.offsetWidth; // Trigger reflow to restart the animation
-  h204.classList.add('reset-animation');
-});
-
-          //check01
-          const CheckElements = document.querySelectorAll('.checkbox');
-          CheckElements.forEach((check01) => {
-            check01.classList.remove('reset-animation');
-            void check01.offsetWidth; // Trigger reflow to restart the animation
-            check01.classList.add('reset-animation');
-          });
-          //check02
-          const CheckElements02 = document.querySelectorAll('.checkbox02');
-          CheckElements02.forEach((check02) => {
-            check02.classList.remove('reset-animation');
-            void check02.offsetWidth; // Trigger reflow to restart the animation
-            check02.classList.add('reset-animation');
-          });
-          //check03
-          const CheckElements03 = document.querySelectorAll('.checkbox03');
-          CheckElements03.forEach((check03) => {
-            check03.classList.remove('reset-animation');
-            void check03.offsetWidth; // Trigger reflow to restart the animation
-            check03.classList.add('reset-animation');
-          });
-           //check04
-           const CheckElements04 = document.querySelectorAll('.checkbox04');
-           CheckElements04.forEach((check04) => {
-             check04.classList.remove('reset-animation');
-             void check04.offsetWidth; // Trigger reflow to restart the animation
-             check04.classList.add('reset-animation');
-           });
-
   
   const slides = [
     {
