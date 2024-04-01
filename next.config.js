e5f2c7091cd1d2bc34ffcec164270ca2285d6187
Ramon Faria <raborzoni@gmail.com>
2024-04-01 08:40:34 -0300

    Correção do CORS.

--- next.config.js
+++ next.config.js
@@ -9,21 +9,7 @@ module.exports = nextConfig
 module.exports = {
   compiler: {
     // ssr and displayName are configured by default
-    styledComponents: true,
-    async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
-    }
+    styledComponents: true
   },
   async rewrites() {
     return [
@@ -33,7 +19,13 @@ module.exports = {
       },
       {
         source: '/contaMessenger/:path*',
-        destination: 'https://api.upsilan.com.br/form_conta/:path*'
+        destination: 'https://api.upsilan.com.br/form_conta/:path*',
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
       },
       {
         source: '/cotacaoMessenger/:path*',
