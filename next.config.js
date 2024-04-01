975475f150db3f9879495e224675dbbc8166d08e
Ramon Faria <raborzoni@gmail.com>
2024-04-01 08:45:19 -0300

    Correção de erro do CORS.

--- next.config.js
+++ next.config.js
@@ -19,13 +19,13 @@ module.exports = {
       },
       {
         source: '/contaMessenger/:path*',
-        destination: 'https://api.upsilan.com.br/form_conta/:path*',
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
+        ],
+        destination: 'https://api.upsilan.com.br/form_conta/:path*'
       },
       {
         source: '/cotacaoMessenger/:path*',
