f8bbccb378a0fb267ac52c9773f9af1e2f1330e9
Ramon Faria <raborzoni@gmail.com>
2024-04-03 08:48:32 -0300

    Edição do headers

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/contaMessenger/:path",
+        source: "/:path",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
